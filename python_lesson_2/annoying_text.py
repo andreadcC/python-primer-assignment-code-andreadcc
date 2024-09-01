332fab0227f97dbe7eb9670d8812ac5c80380d37
andreadcC <johndoe@email.com-global>
2024-09-02 01:57:58 +0800

    Assignment #2 for strings and variable (lesson 2)

---
+++ python_lesson_2/annoying_text.py
@@ -0,0 +1,6 @@
+original_text='Sandrock! '
+shouting_text=original_text.upper()
+annoying_text=(shouting_text*10)
+char_annoyingtext=len(annoying_text)
+print(f"""My annoying text is {annoying_text}")
+It has {char_annoyingtext} characters.""")
